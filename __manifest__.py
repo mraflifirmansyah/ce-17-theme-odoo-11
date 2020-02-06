9e551bb81eec7e57d427232b80d08972c6876d70
M. Rafli Firmansyah <mraflifirmansyah@outlook.com>
2020-02-06 15:22:41 +0700

    Update structure

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "CE Dashboard",
+    "summary": "Customized dashboard look by CE 2017",
+    "version": "11.0.0.1",
+    "category": "Design",
+    "website": "http://tekkom.pens.ac.id",
+    "author": "M. Rafli Firmansyah (2210171003)",
+    "license": "LGPL-3",
+    "installable": True,
+    "data": [
+        'views/importer.xml',
+    ],
+}

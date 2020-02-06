b3dbfa5ce0b0f8efea4a8b534ea3838e251ffe0e
M. Rafli Firmansyah <mraflifirmansyah@outlook.com>
2020-02-06 23:47:32 +0700

    Add onclick function

--- static/src/js/dashboard.js
+++ static/src/js/dashboard.js
@@ -3,8 +3,8 @@ var drawerState = "close";
 function modifyDrawerToggler(){
     var aToggler = document.getElementsByClassName("app-drawer-toggle")[0];
     var btnToggler = document.createElement("button");
+    btnToggler.id = "drawerToggleBtn";
     btnToggler.className = "drawer-toggle navbar-collapse collapse btn btn-default app-drawer-toggle";
-    btnToggler.onclick = "toggleDrawerView()";
     btnToggler.innerHTML = "<span class=\"sr-only\">Toggle App Drawer</span> \
                                 <i class=\"fa fa-th fa-lg app-drawer-icon-open\" \
                                 t-translation=\"off\" \
@@ -32,6 +32,7 @@ function toggleDrawerView(){
 
 window.onload = function(){
     this.modifyDrawerToggler();
+    this.document.getElementById("drawerToggleBtn").onclick = function(){toggleDrawerView()};
 
     return;
 }
